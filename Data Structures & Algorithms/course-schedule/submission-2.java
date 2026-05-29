fb0aab7901c82081bc9d933a00812b761ce4b95d
MD Sameem <mohammadsameem559@gmail.com>
2026-05-29 11:38:19 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=1;i<=n;i++) adj.add(new ArrayList<>());
+        int[] indegree = new int[n];
+        for(int i=0;i<pre.length;i++){
+            int a = pre[i][0], b = pre[i][1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.add(i);
+        }
+
+        while(q.size()>0){
+            int node = q.remove();
+            ans.add(node);
+            for(int ele : adj.get(node)){
+                indegree[ele]--;
+                if(indegree[ele]==0) q.add(ele);
+            }
+        }
+
+        return ans.size()==n;
+    }
+}
