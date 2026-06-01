85f30ee9039a35ecdcfb7abbf200b38f0169a600
MD Sameem <mohammadsameem559@gmail.com>
2026-06-01 07:40:03 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+
+        for(int[] edge : edges){
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+            boolean[] vis = new boolean[n+1];
+
+            if(dfs(u, -1, adj, vis)) return edge;
+        }
+
+        return new int[0];
+    }
+
+    private boolean dfs(int node, int parent, List<List<Integer>> adj, boolean[] vis){
+        if(vis[node]) return true;
+
+        vis[node] = true;
+        for(int nei : adj.get(node)){
+            if(nei == parent) continue;
+            if(dfs(nei, node, adj, vis)) return true;
+        }
+
+        return false;
+    }
+}
