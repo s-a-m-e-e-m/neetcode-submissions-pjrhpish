ad03bdedd6bb858abfa1ed4a882f5f77f81dbd6c
MD Sameem <mohammadsameem559@gmail.com>
2026-06-02 11:26:15 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    private int climb(int n, int[] dp){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = climb(n-1, dp) + climb(n-2, dp);
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return climb(n, dp);
+    }
+}
