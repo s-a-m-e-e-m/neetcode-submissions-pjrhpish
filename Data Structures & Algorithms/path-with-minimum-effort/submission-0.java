118ba390ba0b72319f16dfffee31eb550733949e
MD Sameem <mohammadsameem559@gmail.com>
2026-06-04 07:15:33 +0530

    Add: path-with-minimum-effort - submission-0

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+        int[][] dist = new int[m][n];
+        for(int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+        dist[0][0] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0]-b[0]);
+        pq.offer(new int[]{0, 0, 0}); // {diff, row, col}
+
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int diff = curr[0], r = curr[1], c = curr[2];
+
+            if(r==m-1 && c==n-1) return diff;
+
+            if(dist[r][c] < diff) continue;
+
+            for(int[] d : dirs){
+                int nr = r+d[0], nc = c+d[1];
+
+                if(nr<0 || nc<0 || nr>=m || nc>=n) continue;
+
+                int newDiff = Math.max(diff, Math.abs(heights[r][c]-heights[nr][nc]));
+                if(newDiff < dist[nr][nc]){
+                    dist[nr][nc] = newDiff;
+                    pq.offer(new int[]{newDiff, nr, nc});
+                }
+            }
+        }
+
+        return 0;
+    }
+}
