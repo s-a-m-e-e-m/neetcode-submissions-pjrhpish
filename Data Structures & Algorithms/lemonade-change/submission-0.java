019cc3db4e4149af693b76bf8e7fb78baf45f807
MD Sameem <mohammadsameem559@gmail.com>
2026-07-03 07:50:53 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for(int b : bills){
+            if(b==5) five++;
+            else if(b==10){
+                ten++;
+                if(five>0) five--;
+                else return false;
+            }
+            else{
+                if(five>0 && ten>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}
